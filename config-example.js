bbd26ac5b8320141823f329e2fc3f20b8f75dec2
Jonathan Wise <57234292+jwise-mfg@users.noreply.github.com>
2023-06-26 13:29:02 -0400

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -12,7 +12,7 @@ config = {
     user: { //These are defaults that the user can over-ride. Get them from your SMIP instance.
         "smipUrl": "https://demo.cesmii.net/graphql",
         "authenticator": "YOUR_AUTHENTICATOR",
-        "password": "YOUR_AUTHENTICATOR_PASSWORD",
+        "password": "YOUR_AUTHENTICATOR_PASSWORD",    //For test only -- do not hard-code a password in prod. It will be visible to any client!
         "username": "YOUR_AUTHENTICATOR_USERNAME",
         "role": "YOUR_AUTHENTICATOR_ROLE",
         "modelParentId": 28221,   //the smip instance id of the parent
